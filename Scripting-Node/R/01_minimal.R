2fca230688e908d687312da21043d6a1f357db8a
Ahmad <Alamiri.Ahmad@Gmail.com>
2025-01-02 20:42:26 -0500

    Initial Draft

--- Scripting-Node/R/01_minimal.R
+++ Scripting-Node/R/01_minimal.R
@@ -16,7 +16,7 @@ library(rjson)
 args <- commandArgs()
 
 
-# We are only interested in the 6th argument, which contains the location of the would be newly-created node_args.json file. 
+# We are only interested in the 6th argument, which contains the location of the would be newly-created 'node_args.json' file. 
 # This file contains essential information about the exported data, including location(s) of the exported text files as well as the row IDs, columns, and column attributes of the exported tables.
 # Define Input File ('node_args.json' file) - located in the 6th argument of the variable 'args'.
 input_file <- args[6]
